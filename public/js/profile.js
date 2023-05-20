e0cdb455788fc4c2073559e82088f36c0703db9e
linotmike <128604219+linotmike@users.noreply.github.com>
2023-05-19 18:28:26 -0700

    updated profile.js

---
+++ public/js/profile.js
@@ -0,0 +1,15 @@
+const delbtn = document.querySelectorAll("#del-post");
+delbtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.value;
+    fetch(`/api/topics/${value}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.ok) {
+        location.reload();
+      } else {
+        alert("unable to delete");
+      }
+    });
+  });
+});
